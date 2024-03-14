99d1c3eac27b5a6bc8da62c6f3b516f0869b1c7b
nodes <159713604+LNodesL@users.noreply.github.com>
2024-03-14 09:34:12 -0400

    Create windows.h

---
+++ windows.h
@@ -0,0 +1,88 @@
+#ifndef CUSTOM_WINDOWS_H
+#define CUSTOM_WINDOWS_H
+
+#include <stddef.h> // For size_t and NULL
+#include "winternl.h" // local file in Lucid/ folder
+
+// Define Windows data types used
+typedef unsigned long       DWORD;
+typedef int                 BOOL;
+typedef unsigned char       BYTE;
+typedef unsigned short      WORD;
+typedef float               FLOAT;
+typedef FLOAT               *PFLOAT;
+typedef BOOL                *PBOOL;
+typedef BOOL                *LPBOOL;
+typedef BYTE                *PBYTE;
+typedef BYTE                *LPBYTE;
+typedef int                 *PINT;
+typedef int                 *LPINT;
+typedef WORD                *PWORD;
+typedef WORD                *LPWORD;
+typedef long                *LPLONG;
+typedef DWORD               *PDWORD;
+typedef DWORD               *LPDWORD;
+typedef void                *PVOID;
+typedef void                *LPVOID;
+typedef const void          *LPCVOID;
+typedef int                 INT;
+typedef unsigned int        UINT;
+typedef unsigned int        *PUINT;
+typedef size_t              SIZE_T;
+typedef SIZE_T              *PSIZE_T;
+typedef long                NTSTATUS;
+typedef NTSTATUS            *PNTSTATUS;
+
+// Define macros
+#ifndef STATUS_SUCCESS
+#define STATUS_SUCCESS ((NTSTATUS)0x00000000L)
+#endif
+
+// Memory allocation flags
+#define MEM_COMMIT              0x1000
+#define MEM_RESERVE             0x2000
+#define MEM_RELEASE             0x8000
+#define PAGE_READWRITE          0x04
+#define PAGE_EXECUTE_READWRITE  0x40
+
+typedef enum _PROCESSINFOCLASS {
+    ProcessBasicInformation = 0,
+    ProcessDebugPort = 7,
+    ProcessWow64Information = 26,
+    ProcessImageFileName = 27,
+    ProcessBreakOnTermination = 29
+} PROCESSINFOCLASS;
+
+typedef struct _PROCESS_BASIC_INFORMATION {
+    NTSTATUS ExitStatus;
+    PVOID PebBaseAddress;
+    ULONG_PTR AffinityMask;
+    LONG BasePriority;
+    ULONG_PTR UniqueProcessId;
+    ULONG_PTR InheritedFromUniqueProcessId;
+} PROCESS_BASIC_INFORMATION, *PPROCESS_BASIC_INFORMATION;
+
+typedef NTSTATUS (NTAPI *PFN_NtAllocateVirtualMemory)(
+    HANDLE ProcessHandle,
+    PVOID* BaseAddress,
+    ULONG_PTR ZeroBits,
+    PSIZE_T RegionSize,
+    ULONG AllocationType,
+    ULONG Protect);
+
+typedef NTSTATUS (NTAPI *PFN_NtFreeVirtualMemory)(
+    HANDLE ProcessHandle,
+    PVOID* BaseAddress,
+    PSIZE_T RegionSize,
+    ULONG FreeType);
+
+typedef NTSTATUS (NTAPI *PFN_NtQueryInformationProcess)(
+    HANDLE ProcessHandle,
+    PROCESSINFOCLASS ProcessInformationClass,
+    PVOID ProcessInformation,
+    ULONG ProcessInformationLength,
+    PULONG ReturnLength);
+
+// You may need to declare any other functions or types that are used by your MemoryManager.
+
+#endif // CUSTOM_WINDOWS_H
